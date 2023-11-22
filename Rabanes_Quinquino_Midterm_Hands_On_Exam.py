e6b8d859bcb82ae7bf1b36ef35a2b99a4d19ed3f
sicnarFderflA <144906631+sicnarFderflA@users.noreply.github.com>
2023-11-22 20:28:43 +0800

    Add files via upload

--- Rabanes_Quinquino_Midterm_Hands_On_Exam.py
+++ Rabanes_Quinquino_Midterm_Hands_On_Exam.py
@@ -5,6 +5,7 @@ num2 = random.randint(1 , 99)
 
 q1 = int(input(f"What is {num1} + {num2}?"))
 a1 = num1 + num2
+print(f"{num1} + {num2} = {a1}")
 
 if q1 == a1:
     print("Your Answer is Correct!")
@@ -20,6 +21,7 @@ num2 = random.randint(1 , 99)
 
 q2 = int(input(f"What is {num1} - {num2}?"))
 a2 = num1 - num2
+print(f"{num1} - {num2} = {a2}")
 
 if q2 == a2:
     print("Your Answer is Correct!")
@@ -35,6 +37,7 @@ num2 = random.randint(1 , 99)
 
 q3 = int(input(f"What is {num1} x {num2}?"))
 a3 = num1 * num2
+print(f"{num1} x {num2} = {a3}")
 
 if q3 == a3:
     print("Your Answer is Correct!")
@@ -50,6 +53,7 @@ num2 = random.randint(1 , 99)
 
 q4 = int(input(f"What is {num1} / {num2}?"))
 a4 = num1 / num2
+print(f"{num1} / {num2} = {a4}")
 
 if q4 == a4:
     print("Your Answer is Correct!")
